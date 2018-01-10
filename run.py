d6a2a2bcc04517b0b3a1cd4082b2ad1be3e436f0
yuanjun <yuanjun@papayamobile.com>
2018-01-10 15:59:57 +0800

    run

--- run.py
+++ run.py
@@ -22,8 +22,6 @@ def client1(message):
 
 def callback(name, id, action):
     result = "%s,%s,%s" % (name, id, action)
-    # print "result", result
-    # client(buy_socket, result)
     client1(result)
 
 
@@ -34,13 +32,13 @@ def finish1():
     HOST = '192.168.2.19'  # The remote host
     PORT = 9999  # The same port as used by the server
     while True:
-        print("beginbeginbeginbeginbeginbeginbeginbeginbeginbegin")
+        print("beginbegin")
         finish_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         finish_sock.connect((HOST, PORT))
         try:
             finish_sock.sendall("complete")
             finish_sock.recv(1024)
-            print("finishfinishfinishfinishfinishfinishfinishfinishfinishfinishfinish")
+            print("finishfinish")
         finally:
             # cls.stop()
             face_ready.set()
@@ -61,17 +59,13 @@ def fun():
     for face in face_list:
         print(face)
         image = face_recognition.load_image_file("face_images/%s" % face)
-        face_locations = face_recognition.face_locations(image[:, :, ::-1], number_of_times_to_upsample)
+        face_locations = face_recognition.face_locations(image, number_of_times_to_upsample)
         face_encoding = face_recognition.face_encodings(image, face_locations, num_jitters)[0]
         name = face.split("_")[0]
         user_id = face.split("_")[1]
         face_encoding_list.append([name, user_id, face_encoding])
 
     print("end example")
-    # Initialize some variables
-    # face_locations = []
-    # face_encodings = []
-    # face_names = []
     process_this_frame = True
     face_names = []
 
@@ -93,35 +87,34 @@ def fun():
         rgb_small_frame = small_frame[:, :, ::-1]
         face_locations = []
         user_id = -1
+        most_small_distance = 1
+        socket_name = "Unknown"
+        socket_user_id = -1
         # Only process every other frame of video to save time
         if process_this_frame:
             # Find all the faces and face encodings in the current frame of video
             face_locations = face_recognition.face_locations(rgb_small_frame, number_of_times_to_upsample)
             face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations, num_jitters)
-            print("LKJHGFDSA", len(face_encodings))
+            print("face count", len(face_encodings))
 
             for face_encoding in face_encodings:
                 name = "Unknown"
                 user_id = -1
                 # See if the face is a match for the known face(s)
-                current_match_score = 0.35
+                current_match_score = 0.37
                 match_scores = face_recognition.face_distance([i[2] for i in face_encoding_list], face_encoding)
                 for i, match_score in enumerate(match_scores):
                     if match_score < current_match_score:
+                        current_match_score = match_score
                         name = face_encoding_list[i][0]
                         user_id = face_encoding_list[i][1]
+                    if match_score < most_small_distance:
+                        most_small_distance = match_score
+                        socket_name = name
+                        socket_user_id = user_id
 
-                # match = face_recognition.compare_faces(
-                #     [i[2] for i in face_encoding_list], face_encoding, 0.33)
-                #
-                # for i in range(len(match)):
-                #     if match[i]:
-                #         name = face_encoding_list[i][0]
-                #         user_id = face_encoding_list[i][1]
-                # print("match count:", match.count(True))
                 face_names.append(name)
                 print(face_names)
-        # process_this_frame = not process_this_frame
 
         for (top, right, bottom, left), name in zip(face_locations, face_names):
             top *= 4
@@ -136,12 +129,12 @@ def fun():
         cv2.imshow("Face", frame)
         cv2.waitKey(1)
 
-        if len(face_names) > 0:
-            print(user_id)
+        if socket_user_id != -1:
+            print(socket_user_id)
             try:
                 face_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 face_sock.connect((HOST, PORT))
-                face_sock.sendall("0,%s,0" % user_id)
+                face_sock.sendall("0,%s,0" % socket_user_id)
                 face_ready.clear()
             finally:
                 face_sock.close()
@@ -151,7 +144,4 @@ def fun():
 finish_thread = threading.Thread(target=finish1)
 finish_thread.start()
 
-# jt_thread = threading.Thread(target=fun)
-# jt_thread.start()
 fun()
-# finish(finish_sock)
