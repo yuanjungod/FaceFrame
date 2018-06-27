c76397bf8162ab5d11c3c7325d02e36f202fbcd6
Happy <happy@HappydeMacBook-Pro.local>
2018-06-27 19:23:55 +0800

    error Message

--- web_service.py
+++ web_service.py
@@ -270,6 +270,8 @@ def face_search():
     for search_group_id in search_group_id_list.split(","):
         if not re.match("^[A-Za-z0-9_-]*$", search_group_id):
             return jsonify({"errorMessage": "search_group_id_list contain illegal character"})
+        if not search_group_id not in feature_dict[appid]:
+            return jsonify({"errorMessage": "search_group_id_list invalid"})
 
     max_user_num = request.values.get("max_user_num")
     if max_user_num is None:
